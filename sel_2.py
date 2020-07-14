a119e8f5c066947bf955da9c33811b9dfa8e0f05
iamprithvishetty <48802728+iamprithvishetty@users.noreply.github.com>
2020-07-15 04:23:51 +0530

    Update sel_2.py

--- sel_2.py
+++ sel_2.py
@@ -1,30 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-Name="Papa"
-string="Good Morning Papa"
+Name="Papa" #Name of the Receiver CapsSensitive
+string="Good Morning Papa" #Message to be sent
 
-driver=webdriver.Chrome(executable_path="C:\Prithvi\Whatsapp\chromedriver_win32 (1)\chromedriver.exe")
-driver.get("https://web.whatsapp.com/")
-#print(driver.title)
-#print(driver.current_url)
-while True:
+driver=webdriver.Chrome(executable_path="C:\Prithvi\Whatsapp\chromedriver_win32 (1)\chromedriver.exe") #chrome driver path
+driver.get("https://web.whatsapp.com/") #url for whatsapp
+#print(driver.title)  #prints title of the page
+#print(driver.current_url) #prints url of the page
+while True: #keep running until break
     try:
-        driver.find_element_by_xpath('//button[@class="_3e4VU"]').click()
-        find_box = driver.find_element_by_xpath('//div[@class="_3FRCZ copyable-text selectable-text"]')
-        find_box.send_keys(Name)
-        #find_box.send_keys(Keys.ENTER)
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@title="'+Name+'"]').click()
-        #text_area=driver.find_elements_by_xpath('//div[contains(text(), "' + text + '")]')
+        driver.find_element_by_xpath('//button[@class="_3e4VU"]').click() #Finding Name in order to send message 1)Clicking Search Button
+        find_box = driver.find_element_by_xpath('//div[@class="_3FRCZ copyable-text selectable-text"]') #Finding Name in order to send message 2)Creating a message box to insaert message
+        find_box.send_keys(Name) # entering receivers name
+        #find_box.send_keys(Keys.ENTER) #Press Enter Key
+        time.sleep(2) #Sleeps for 2s
+        driver.find_element_by_xpath('//*[@title="'+Name+'"]').click() #Sending Messages 1)Entering the profile
+        #text_area=driver.find_elements_by_xpath('//div[contains(text(), "' + text + '")]') 
         for i in range(5):
-            message_box= driver.find_element_by_xpath('//div[@class="_3uMse"]')
-            message_box.send_keys(string)
-            message_box= driver.find_element_by_xpath('//button[@class="_1U1xa"]')
+            message_box= driver.find_element_by_xpath('//div[@class="_3uMse"]') #Sending Messages 2)Selecting the type box
+            message_box.send_keys(string) #Sending Messages 3)Writing String
+            message_box= driver.find_element_by_xpath('//button[@class="_1U1xa"]') #Sending Messages 4)Finding send button and then clicking
             message_box.click()
         break
 
     except:
-        print("Waiting For Whatsapp to open")
+        print("Waiting For Whatsapp to open") #incase whatsapp isn,t open 
         time.sleep(5)
-driver.close()
+driver.close() #shuts off the connection
